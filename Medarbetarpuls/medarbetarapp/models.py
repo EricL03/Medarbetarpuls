08d55d225d9709983972481e924bfb3b5472b77c
Jonathan Elenstål <jonel916@student.liu.se>
2025-04-02 13:01:16 +0200

    Functionality to create account

--- Medarbetarpuls/medarbetarapp/models.py
+++ Medarbetarpuls/medarbetarapp/models.py
@@ -1,3 +1,18 @@
+from __future__ import annotations
 from django.db import models
+from django.db.models.manager import BaseManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+import logging
+from typing import cast
+
 
 # Create your models here.
+
+
+class EmailList(models.Model):
+    email = models.EmailField(unique=True)
+    
--- Medarbetarpuls/medarbetarapp/views.py
+++ Medarbetarpuls/medarbetarapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import logging
+import models
 
 logger = logging.getLogger(__name__)
 
@@ -10,3 +11,15 @@ def index(request):
     logger.error("Testing error!!!")
 
     return render(request, "index.html")
+
+def create_acc_view(request):
+    if request.method == 'POST':
+        if request.headers.get('HX-Request'):
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            models.CustomUserManager(email,name,password)
+            models.EmailList(email=email)
+
+    return render(request, 'create_acc.html')
+
