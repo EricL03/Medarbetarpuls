7147ed99c470022e8770c7eee514664cf2b56dd0
Johanna Lindquist <hannalin21@gmail.com>
2025-04-02 14:53:13 +0200

    settings_user.html fixed, should fix delete button functionality

--- Medarbetarpuls/medarbetarapp/views.py
+++ Medarbetarpuls/medarbetarapp/views.py
@@ -11,63 +11,82 @@ def index_view(request):
 
     return render(request, "index.html")
 
+
 def add_employee_view(request):
-    return render(request, 'add_employee.html')
+    return render(request, "add_employee.html")
+
 
 def analysis_view(request):
-    return render(request, 'analysis.html')
+    return render(request, "analysis.html")
+
 
 def answer_survey_view(request):
-    return render(request, 'answer_survey.html')
+    return render(request, "answer_survey.html")
+
 
 def authentication_acc_view(request):
-    return render(request, 'authentication_acc.html')
+    return render(request, "authentication_acc.html")
+
 
 def authentication_org_view(request):
-    return render(request, 'authentication_org.html')
+    return render(request, "authentication_org.html")
+
 
 def create_acc_view(request):
-    return render(request, 'create_acc.html')
+    return render(request, "create_acc.html")
+
 
 def create_org_view(request):
-    return render(request, 'create_org.html')
+    return render(request, "create_org.html")
+
 
 def create_survey_view(request):
-    return render(request, 'create_survey.html')
+    return render(request, "create_survey.html")
+
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, "login.html")
+
 
 def my_org_view(request):
-    return render(request, 'my_org.html')
+    return render(request, "my_org.html")
+
 
 def my_results_view(request):
-    return render(request, 'my_results.html')
+    return render(request, "my_results.html")
+
 
 def my_surveys_view(request):
-    return render(request, 'my_surveys.html')
+    return render(request, "my_surveys.html")
+
 
 def publish_survey_view(request):
-    return render(request, 'publish_survey.html')
+    return render(request, "publish_survey.html")
+
 
 def settings_admin_view(request):
-    return render(request, 'settings_admin.html')
+    return render(request, "settings_admin.html")
+
 
 def settings_user_view(request):
-    return render(request, 'settings_user.html')
+    return render(request, "settings_user.html", {"user": request.user})
+
 
 def start_admin_view(request):
-    return render(request, 'start_admin.html')
+    return render(request, "start_admin.html")
+
 
 def start_user_view(request):
-    return render(request, 'start_user.html')
+    return render(request, "start_user.html")
+
 
 def survey_result_view(request):
-    return render(request, 'survey_result.html')
+    return render(request, "survey_result.html")
+
 
 def survey_status_view(request):
-    return render(request, 'survey_status.html')
+    return render(request, "survey_status.html")
+
 
 def unanswered_surveys_view(request):
-    return render(request, 'unanswered_surveys.html')
-
+    return render(request, "unanswered_surveys.html")
