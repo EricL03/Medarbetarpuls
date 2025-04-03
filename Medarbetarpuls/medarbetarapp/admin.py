02bc79cebfb9593cea7c608ed5f21dc1b65824a0
Eric <eric.lofgren03@gmail.com>
2025-04-03 12:30:07 +0200

    Added org model to admin-panel for easier debugging

--- Medarbetarpuls/medarbetarapp/admin.py
+++ Medarbetarpuls/medarbetarapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser  # Import your CustomUser model
+from .models import CustomUser, Organization  # Import your CustomUser model
 
 
 class CustomUserAdmin(UserAdmin):
@@ -41,3 +41,24 @@ class CustomUserAdmin(UserAdmin):
 
 # Register the CustomUser model
 admin.site.register(CustomUser, CustomUserAdmin)
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_admins", "get_employee_groups", "get_question_bank", "get_survey_templates")
+    search_fields = ("name",)
+
+    @admin.display(description="Admins")
+    def get_admins(self, obj):
+        return ", ".join([admin.email for admin in obj.admins.all()])
+
+    @admin.display(description="Employee Group")
+    def get_employee_groups(self, obj):
+        return ", ".join([group.name for group in obj.employee_groups.all()])
+
+    @admin.display(description="Question Bank")
+    def get_question_bank(self, obj):
+        return ", ".join([question.question for question in obj.question_bank.all()])
+
+    @admin.display(description="Survey Templates")
+    def get_survey_templates(self, obj):
+        return ", ".join([template.name for template in obj.survey_template_bank.all()])
